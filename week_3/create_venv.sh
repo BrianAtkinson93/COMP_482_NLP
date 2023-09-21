9d6b0b4cb51b89802dbcf66e24ed8eaea1f0c4f8
Brian Atkinson <76421912+BrianAtkinson93@users.noreply.github.com>
2023-09-20 20:18:03 -0700

    Added basic script to create venv

---
+++ week_3/create_venv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Using the system python3 (you can chose...) create new venv
+python3 -m venv week_3_venv
+
+# Source the activate script to activate the venv
+source week_3_venv/bin/activate
+
+
